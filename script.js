988165bfe928421f0d6fa77a2c7eb55c41fd352c
Leonardo Santos Severino <leonardossev@gmail.com>
2020-02-01 14:49:14 -0300

    Adds script file

---
+++ script.js
@@ -0,0 +1,25 @@
+function calcular(event) {
+  event.preventDefault();
+  const numPessoas = pegarValor('#people');
+  const numHoras = pegarValor('#hours');
+
+  const numComida = (100 * numPessoas)/numHoras;
+  const numLatinha = (1 * numPessoas)/numHoras;
+  const numRefri = (100 * numPessoas)/numHoras;
+
+  exibirValores(numComida, numLatinha, numRefri);
+}
+
+function pegarValor(selector) {
+  const input = document.querySelector(selector);
+
+  return input.value;
+}
+
+function exibirValores(comida, latinha, refri) {
+  document.querySelector('#numComida').innerHTML = `${comida}g de comida.`;
+  document.querySelector('#numLatinha').innerHTML = `${latinha} latinha(s) de cerveja.`;
+  document.querySelector('#numRefri').innerHTML = `${refri}ml de refrigerante.`;
+
+  document.querySelector('#result').style.display = 'block';
+}
